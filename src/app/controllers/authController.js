8f58dbbbee3139a437e67b15d5b0a740279991e4
duy luu <duyluu51@gmail.com>
2022-12-02 14:29:25 +0700

    handle refreshToken

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -1,7 +1,7 @@
 const bcrypt = require("bcrypt");
 const userModel = require("../models/usersModels");
 const authMethod = require("../method/authMethods");
-const jwtVariable=require("../../constant").jwtVariable
+const jwtVariable = require("../../constant").jwtVariable;
 const randToken = require("rand-token");
 
 // Khai báo const
@@ -9,7 +9,7 @@ const SALT_ROUNDS = 10;
 // end
 
 class authController {
-  // [post] register
+  //1. [post] register
   register = async (req, res) => {
     const username = req.body.username.toLowerCase();
     const userList = await userModel.find({ username });
@@ -38,17 +38,16 @@ class authController {
     }
   };
 
-  // [post] log-in
+  //2. [post] log-in
   login = async (req, res) => {
     const username = req.body.username.toLowerCase();
     const password = req.body.password;
 
     const userList = await userModel.find({ username });
-    if (!userList.length === 0) {
+    if (userList.length === 0) {
       return res.status(401).send("Tên đăng nhập không tồn tại.");
     }
     const user = userList[0];
-    console.log(user);
 
     const isPasswordValid = bcrypt.compareSync(password, user.password);
     if (!isPasswordValid) {
@@ -59,7 +58,7 @@ class authController {
     const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
 
     const dataForAccessToken = {
-      username: user.username,
+      username: user._id,
     };
     const accessToken = await authMethod.generateToken(
       dataForAccessToken,
@@ -77,7 +76,7 @@ class authController {
 
     if (!user.refreshToken) {
       // Nếu user này chưa có refresh token thì lưu refresh token đó vào database
-      await userModel.updateOne({ username:user.username}, {...user,refreshToken });
+      await userModel.findOneAndUpdate({ _id: user._id }, { refreshToken });
     } else {
       // Nếu user này đã có refresh token thì lấy refresh token đó từ database
       refreshToken = user.refreshToken;
@@ -88,6 +87,65 @@ class authController {
       accessToken,
       refreshToken,
       user,
+    });
+  };
+
+  //3. [post] refreshToken
+  refreshToken = async (req, res) => {
+    // Lấy access token từ header
+    const accessTokenFromHeader = req.headers.x_authorization;
+    if (!accessTokenFromHeader) {
+      return res.status(400).send("Không tìm thấy access token.");
+    }
+
+    // Lấy refresh token từ body
+    const refreshTokenFromBody = req.body.refreshToken;
+    if (!refreshTokenFromBody) {
+      return res.status(400).send("Không tìm thấy refresh token.");
+    }
+
+    const accessTokenSecret =
+      process.env.ACCESS_TOKEN_SECRET || jwtVariable.accessTokenSecret;
+    const accessTokenLife =
+      process.env.ACCESS_TOKEN_LIFE || jwtVariable.accessTokenLife;
+
+    // Decode access token đó
+    const decoded = await authMethod.decodeToken(
+      accessTokenFromHeader,
+      accessTokenSecret
+    );
+    if (!decoded) {
+      return res.status(400).send("Access token không hợp lệ.");
+    }
+
+    const username = decoded.payload.username; // Lấy username từ payload
+
+    const user = await userModel.getUser(username);
+    if (!user) {
+      return res.status(401).send("User không tồn tại.");
+    }
+
+    if (refreshTokenFromBody !== user.refreshToken) {
+      return res.status(400).send("Refresh token không hợp lệ.");
+    }
+
+    // Tạo access token mới
+    const dataForAccessToken = {
+      username,
+    };
+
+    const accessToken = await authMethod.generateToken(
+      dataForAccessToken,
+      accessTokenSecret,
+      accessTokenLife
+    );
+    if (!accessToken) {
+      return res
+        .status(400)
+        .send("Tạo access token không thành công, vui lòng thử lại.");
+    }
+    return res.json({
+      accessToken,
     });
   };
 }
--- src/app/models/StudentModel.js
+++ src/app/models/StudentModel.js
@@ -4,15 +4,20 @@ const slug = require("mongoose-slug-generator");
 const Schema = mongoose.Schema;
 const mongooseDelete = require("mongoose-delete");
 
-const Student = new Schema({
-  name: { type: String, required: true },
-  class: { type: String, required: true },
-  school: { type: String, required: true },
-  phoneNumber: { type: String, required: true },
-  shift: { type: String, required: true },
-  year: { type: String, required: true },
-  idStudent: { type: String, required: true },
-});
+const Student = new Schema(
+  {
+    name: { type: String, required: true },
+    class: { type: String, required: true },
+    school: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    shift: { type: String, required: true },
+    year: { type: String, required: true },
+    idStudent: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 // Add plugin
 mongoose.plugin(slug);
