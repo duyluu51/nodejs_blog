0db5895e04bbd2b8ec95b92dc5814b4bad10d215
duy luu <duyluu51@gmail.com>
2022-12-07 19:29:18 +0700

    fix url get course

--- src/routes/courseRoutes.js
+++ src/routes/courseRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const courseController = require("../app/controllers/courseController");
-const isAuth =require('../app/middlewares/authMiddlewares').isAuth
+const isAuth = require("../app/middlewares/authMiddlewares").isAuth;
 
-router.get("/getCourse",isAuth, courseController.getCourse);
-router.post("/create",isAuth, courseController.create);
+router.get("/getCourse", courseController.getCourse);
+router.post("/create", isAuth, courseController.create);
 
 module.exports = router;
