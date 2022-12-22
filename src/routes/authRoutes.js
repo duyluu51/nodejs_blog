a419f9c6e0d718983548e7f0e6ecf7e17aab38a9
duy luu <duyluu51@gmail.com>
2022-12-22 15:08:35 +0700

    bug fix JWT for auth/register

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-
+const isAuth = require("../app/middlewares/authMiddlewares").isAuth;
 const authController = require("../app/controllers/authController.js");
 
-router.post("/register", authController.register);
+router.post("/register", isAuth, authController.register);
 router.post("/login", authController.login);
-router.post("/refresh", authController.refreshToken);
+router.post("/refresh", isAuth, authController.refreshToken);
 
 module.exports = router;
